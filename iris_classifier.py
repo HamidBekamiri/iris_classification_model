3724d40d906294fbd14ec4e5c0932efe85d96685
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-06 16:05:56 +0200

    Create iris_classifier.py

---
+++ iris_classifier.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+iris = pd.read_csv(url, header=None, names=column_names)
+
+# Use the first 100 rows for training and testing
+iris = iris.head(100)
+
+# Prepare the data
+X = iris.iloc[:, :-1].values
+y = iris.iloc[:, -1].values
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+# Test the model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
