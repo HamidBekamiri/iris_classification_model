7c92fe168187669aec66d6d3231d99eba6e07e9f
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-06 23:16:17 +0200

    Update iris_classifier.py

--- iris_classifier.py
+++ iris_classifier.py
@@ -17,7 +17,7 @@ X = iris.iloc[:, :-1].values
 y = iris.iloc[:, -1].values
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Train the model
 model = KNeighborsClassifier(n_neighbors=10)
