81b09be28b240974f0d68b667c0de925efb77248
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-07 13:30:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,29 @@
-import os
-import joblib
-import pandas as pd
-import numpy as np
-import requests
-import zipfile
-import streamlit as st
 
-# Define the name of the artifact to download
-artifact_name = 'my-model'
+import pickle
+# import streamlit as st
+from sklearn.datasets import load_iris
 
-# Set up the API endpoint and headers
-api_url = f'https://api.github.com/repos/HamidBekamiri/iris_classification_model/actions/artifacts'
-headers = {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'token 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'}
+# Load the iris dataset
+iris = load_iris()
 
-# Get the list of artifacts for the repository
-response = requests.get(api_url, headers=headers)
-response_json = response.json()
-artifacts = response_json['artifacts']
+# Load the saved model
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-# Find the ID of the artifact that matches the specified name
-artifact_id = None
-for artifact in artifacts:
-    if artifact['name'] == artifact_name:
-        artifact_id = artifact['id']
-        break
+# Define the prediction function
+def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+    features = [[sepal_length, sepal_width, petal_length, petal_width]]
+    species = model.predict(features)
+    return iris.target_names[species[0]]
 
-# Download the artifact and extract the trained model
-if artifact_id:
-    artifact_url = f'{api_url}/{artifact_id}/zip'
-    response = requests.get(artifact_url, headers=headers)
-    with open(f'{artifact_name}.zip', 'wb') as f:
-        f.write(response.content)
+# Create the Streamlit app
+st.title("Iris Species Prediction")
 
-    with zipfile.ZipFile(f'{artifact_name}.zip', 'r') as zip_ref:
-        for file in zip_ref.namelist():
-            if file.endswith('.joblib'):
-                zip_ref.extract(file)
-                model_path = file
+sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.0)
+sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.0)
+petal_length = st.slider("Petal Length", 1.0, 7.0, 4.0)
+petal_width = st.slider("Petal Width", 0.1, 2.5, 1.0)
 
-    # Load the trained model
-    model = joblib.load(model_path)
-
-    # Load the test dataset
-    test_data = pd.read_csv("test_data.csv")
-
-    # Extract features and target
-    X_test = test_data.drop('target', axis=1)
-    y_test = test_data['target']
-
-    # Make predictions on test dataset
-    y_pred = model.predict(X_test)
-
-    # Compute accuracy
-    accuracy = np.mean(y_pred == y_test)
-
-    # Display results in Streamlit app
-    st.write(f"Accuracy: {accuracy}")
-else:
-    st.write(f"No artifact found with name {artifact_name}")
+if st.button("Predict"):
+    species = predict_species(sepal_length, sepal_width, petal_length, petal_width)
+    st.write(f"The predicted species is {species}.")
